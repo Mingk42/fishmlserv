815356be4f85d199db6fd13cc0160dc6c83e3e7d
Mingk42 <xoals123456t@gmail.com>
2024-09-03 14:32:33 +0900

    tset

--- src/fishmlserv/pred.py
+++ src/fishmlserv/pred.py
@@ -24,3 +24,45 @@ def prediction(l:float=typer.Option(...,"-l"), w:float=typer.Option(...,"-w")):
 
 def run():
     typer.run(prediction)
+
+
+def run_pred(length:float=typer.Option(...,"-l","--length"),weight:float=typer.Option(...,"-w","--weight")):
+
+    typer.run(fish(length,weight))
+
+
+def fish(length:float, weight:float):
+    """
+    어종 판별기
+
+    Args:
+     - length(int): 물고기 길이(cm)
+     - weight(int): 물고기 무게(g)
+
+    Return
+     - dict, 물고기의 종류를 담은 딕셔너리
+    """
+    
+    from fishmlserv.model.manager import get_model_path 
+#    if length>=30:
+#        prediction="도미"
+#    else:
+#        prediction="빙어"
+
+
+    with open(get_model_path(), "rb") as f:
+        fish_model=pickle.load(f)
+        
+    pred=fish_model.predict([[length, weight]])[0]
+
+    CLASSES={
+                0:"빙어",
+                1:"도미"
+            }
+
+    return {
+            "prediction":CLASSES[pred],
+            # "prediction":get_model_path(),
+            "length":length,
+            "weight":weight
+            }
