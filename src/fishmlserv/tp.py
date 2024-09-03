c3ad38f92eb935a50c7753c23aef0ffcee33e6e3
Mingk42 <xoals123456t@gmail.com>
2024-09-03 11:11:13 +0900

    typer added

---
+++ src/fishmlserv/tp.py
@@ -0,0 +1,32 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}")
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
+
+@app.command()
+def prediction(l:float, w:float):
+    from fishmlserv.model.manager import get_model_path
+    import pickle
+
+    with open(get_model_path(),"rb") as f:
+        model=pickle.load(f)
+
+    pred=model.predict(l,w)
+
+    print(pred)
+
+
+if __name__ == "__main__":
+    app()
