599659c53577410886c4ddd8ffa9382c0b4ef9e5
Mingk42 <xoals123456t@gmail.com>
2024-09-03 12:41:12 +0900

    typer modifying

---
+++ src/fishmlserv/pred.py
@@ -0,0 +1,21 @@
+import typer
+
+app = typer.Typer()
+
+
+def prediction(l:float=typer.Option(...,"-l"), w:float=typer.Option(...,"-w")):
+
+    from fishmlserv.model.manager import get_model_path
+    from sklearn.neighbors import KNeighborsClassifier
+    import pickle
+
+    with open(get_model_path(),"rb") as f:
+        model=pickle.load(f)
+
+    pred=model.predict([[l,w]])
+    
+    print(pred)
+
+
+def run():
+    typer.run(prediction)
